d909370b5afbf3f8c512a26515238d37513032e3
koby.hershkovitz <koby.hershkovitz@8b03d896-9aaa-11de-96bd-0376b6bc6ad2>
2009-09-06 18:29:09 +0000

    added technology test apps

---
+++ WebAgentTest1/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WebAgentTest1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFill = new System.Windows.Forms.Button();
+            this.usersDataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.usersDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonFill
+            // 
+            this.buttonFill.Location = new System.Drawing.Point(12, 12);
+            this.buttonFill.Name = "buttonFill";
+            this.buttonFill.Size = new System.Drawing.Size(75, 23);
+            this.buttonFill.TabIndex = 0;
+            this.buttonFill.Text = "Fill";
+            this.buttonFill.UseVisualStyleBackColor = true;
+            this.buttonFill.Click += new System.EventHandler(this.buttonFill_Click);
+            // 
+            // usersDataGrid
+            // 
+            this.usersDataGrid.AllowUserToAddRows = false;
+            this.usersDataGrid.AllowUserToDeleteRows = false;
+            this.usersDataGrid.AllowUserToOrderColumns = true;
+            this.usersDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.usersDataGrid.Location = new System.Drawing.Point(12, 52);
+            this.usersDataGrid.Name = "usersDataGrid";
+            this.usersDataGrid.ReadOnly = true;
+            this.usersDataGrid.Size = new System.Drawing.Size(516, 298);
+            this.usersDataGrid.TabIndex = 1;
+            this.usersDataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 456);
+            this.Controls.Add(this.usersDataGrid);
+            this.Controls.Add(this.buttonFill);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.usersDataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFill;
+        private System.Windows.Forms.DataGridView usersDataGrid;
+    }
+}
+
