03d5719ba7b7e32bc176f0090e0d8a05bd32d6ba
koby.hershkovitz <koby.hershkovitz@8b03d896-9aaa-11de-96bd-0376b6bc6ad2>
2010-04-21 20:59:27 +0000

    script data population completed

--- trunk/AutoWebAgent/awaApplication/Properties/Settings.Designer.cs
+++ trunk/AutoWebAgent/awaApplication/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace awaApplication.Properties {
                 return ((string)(this["awaDB"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=..\\db\\AutoWebAgentDB.sdf;Password=koby3274;Persist Security Info=True" +
+            "")]
+        public string awaDBDev {
+            get {
+                return ((string)(this["awaDBDev"]));
+            }
+        }
     }
 }
